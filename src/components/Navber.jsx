0cc6647e8f0d48023f5c34cd3dd4af6ea03896d5
Abdur Rahman <abdurrahman21266057@gmail.com>
2025-11-27 21:02:23 +0600

    Add Navber component and integrate it into HomeLayout

---
+++ src/components/Navber.jsx
@@ -0,0 +1,30 @@
+
+import { Link } from 'react-router-dom';
+import user from '../assets/user.png'
+ const Navber = () => {
+    return (
+        <div className='flex justify-between items-center my-4'>
+            <div className='' ></div>
+            <div className='nae space-x-5' >
+                <Link to='/'>Home</Link>
+                <Link to='/career'>Career</Link>
+                <Link to='/about'>About</Link>
+                
+
+
+
+            </div>
+            <div className='login' >
+
+                <div className='flex gap-6'>
+
+                  <div >  <img className='' src={user} alt="User" /></div>
+                    <button className='bg-green-500 px-6 py-3 rounded-sm'> <Link to ='/login'>Login</Link> </button>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Navber;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,14 +1,25 @@
 
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
+import Navber from '../components/Navber';
 
 const HomeLayout = () => {
   return (
     <div className="max-:mx-auto">
       <Header />
+      <section className='w-11/12 mx-auto px-2'>
+
+
       <LatestNews />
-      <nav></nav>
-      <main></main>
+      </section>
+      <nav className='w-11/12 mx-auto px-2'>
+      <Navber/>
+      </nav>
+      <main className=' w-11/12 mx-auto px-2 py-2 grid grid-cols-12'>
+<aside className='left col-span-3'> Left Navber </aside>
+<section className='col-span-6'> Main Content</section>
+<aside className='col-span-3'>Right Navber</aside>
+      </main>
     </div>
   );
 };
